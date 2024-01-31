17651d0143c5b293206b6d3825899cd96bfa83f8
Mariem <mariem.ehab@ieee.org>
2024-01-31 12:23:00 -0800

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,19 +6,18 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
+#sudo mkdir -p /data/
+#sudo mkdir -p /data/web_static/
+#sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+#sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 
 echo "Hello from test static" > /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 
 nginx_cong="
 server {
