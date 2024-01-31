b8da2719b437e064fa14abea0c44fda7f3135520
Mariem <mariem.ehab@ieee.org>
2024-01-31 13:21:18 -0800

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-
 # Bash script that sets up your web servers for
 #the deployment of web_static
 
 sudo apt-get -y update
 sudo apt-get -y install nginx
 
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
