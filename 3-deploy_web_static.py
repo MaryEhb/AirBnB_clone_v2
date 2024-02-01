8b315b162dc831ab941560dba15de267406b28f9
Mariem <mariem.ehab@ieee.org>
2024-01-31 18:40:29 -0800

    Task 3 call do_pack outside function

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""3. Full deployment of two servers"""
+
+
+from fabric.api import *
+import os
+import datetime
+env.hosts = ["34.224.62.130", "3.94.213.33"]
+path = None
+
+
+def do_pack():
+    """Compress before sending"""
+    global path
+    try:
+        if path is None:
+            local("mkdir -p versions")
+            created_at = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+            path = "versions/web_static_{}.tgz".format(created_at)
+            command = local("sudo tar -cvzf {} web_static".format(path))
+        return path
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    """distributes an archive to your web servers"""
+
+    if os.path.exists(archive_path) is False:
+        return False
+
+    try:
+        put(archive_path, "/tmp/")
+        filename = archive_path.split("/")[-1]
+        path = "/data/web_static/releases/{}".format(filename.split(".")[0])
+        run("mkdir -p {}/".format(path))
+        run("tar -xzf /tmp/{} -C {}/".format(filename, path))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}/web_static/* {}/".format(path, path))
+        run("rm -rf {}/web_static".format(path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(path))
+        return True
+    except Exception:
+        return False
+
+
+path = do_pack()
+
+
+def deploy():
+    """creates and distributes an archive to your web servers
+    using the function"""
+    if path is None:
+        return False
+    return do_deploy(path)
