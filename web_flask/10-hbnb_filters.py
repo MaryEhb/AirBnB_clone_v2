51a5c2596a1cca13815214d071b4d331b7179a13
Your Name <you@example.com>
2024-02-17 15:24:46 -0800

    Task 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -4,22 +4,18 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.amenity import Amenity
 
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def show_states(id=None):
-    """show states"""
-    states = storage.all(State)
-    try:
-        state = states['State.' + id] if id is not None else None
-    except (KeyError):
-        return render_template('9-states.html', states=None, state=None)
-    return render_template('9-states.html',
-                           states=states.values(), state=state)
-
+@app.route('/hbnb_filters', strict_slashes=False)
+def show_states_aminities(id=None):
+    """Task 11"""
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    return render_template('10-hbnb_filters.html', states=states,
+                           amenities=amenities)
 
 @app.teardown_appcontext
 def teardown_db(exception):
