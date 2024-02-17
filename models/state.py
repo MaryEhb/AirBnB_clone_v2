b27a91d99757fe1e4a77f029524362b48ae3dfcb
Your Name <you@example.com>
2024-02-17 03:36:41 -0800

    Task 7

--- models/state.py
+++ models/state.py
@@ -4,7 +4,6 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
-from models import storage
 from models.city import City
 
 
