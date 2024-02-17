57a55f6b4d79868e49b4749ca0c57230b306927b
Your Name <you@example.com>
2024-02-17 03:25:03 -0800

    Task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -73,4 +73,4 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         if self.__session:
-            self.__session.close()
+            self.__session.remove()
