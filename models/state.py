9f68c66104078c0dab5183d37c4d0e7b113abd76
Your Name <you@example.com>
2024-02-17 04:14:33 -0800

    Task 7

--- models/state.py
+++ models/state.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" holds class State"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import getenv
 from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == "db":
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='all')
+        cities = relationship("City", backref="state")
     else:
         name = ""
 
+        @property
         def cities(self):
             """get cities  linked to state"""
             cities = []
-            cities_dict = storage.all(City)
-            for key, value in cities_dict.items():
-                if self.id == value.state_id:
-                    cities.append(value)
+            storage_cities = models.storage.all(City)
+            for city in storage_cities.values():
+                if city.state_id == self.id:
+                    cities.append(city)
             return cities
+
+    def __init__(self, *args, **kwargs):
+        """init state?"""
+        super().__init__(*args, **kwargs)
+
---
+++ models/stateTEST.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.city import City
+import models
+
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = 'states'
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='all')
+    else:
+        name = ""
+
+        def cities(self):
+            """get cities  linked to state"""
+            cities = []
+            cities_dict = models.storage.all(City)
+            for key, value in cities_dict.items():
+                if self.id == value.state_id:
+                    cities.append(value)
+            return cities
