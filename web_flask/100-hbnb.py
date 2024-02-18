363a17e6208516252ce5e812778a61a5d1c5d835
Mariem <mariem.ehab@ieee.com>
2024-02-18 14:25:25 +0200

    Task 12

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -5,17 +5,19 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+from models.place import Place
 
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def show_states_aminities(id=None):
     """Task 11"""
     states = storage.all(State).values()
     amenities = storage.all(Amenity).values()
-    return render_template('10-hbnb_filters.html', states=states,
-                           amenities=amenities)
+    places = storage.all(Place).values()
+    return render_template('100-hbnb.html', states=states,
+                           amenities=amenities, places=places)
 
 @app.teardown_appcontext
 def teardown_db(exception):
